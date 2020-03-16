27dffc4585da1817aa6f1e0af425b7adf2551211
“Nia <nisom@purdue.edu>
2020-03-15 17:16:17 -0700

    Events are added only is someone on the meeting request is on the event

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,78 @@
 
 package com.google.sps;
 
+import java.util.*;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public final class FindMeetingQuery {
+  private ArrayList<TimeRange> busyTimes;
+  private ArrayList<TimeRange> freeTimes;
+  private ArrayList<String> meetingAttendees;
+  private ArrayList<String> eventAttendees;
+
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+// only add the event to busyTimes if an attendee on the Meeting Request is on the event
+    busyTimes = new ArrayList<TimeRange>();
+    meetingAttendees = new ArrayList<String>();
+    eventAttendees = new ArrayList<String>();
+
+    for (int i = 0; i<request.getAttendees().size(); i++){
+        (request.getAttendees()).forEach(person -> meetingAttendees.add(person));
+    }
+    // for-each loop
+    for (Event currEvent : events) { // for each event
+        for (int i = 0; i<currEvent.getAttendees().size(); i++){
+            (currEvent.getAttendees()).forEach(person -> eventAttendees.add(person));
+        }
+        for (String eventPerson : eventAttendees){
+            if (meetingAttendees.contains(eventPerson)){ // if event person is in meeting request
+                busyTimes.add(currEvent.getWhen());
+                break; // move to next event
+                }
+            // if it gets here, check the next person in the event
+        }
+    // break goes here. on to next event
+    }
+                
+     // 1. Sort the inputted ranges by start time. Smallest Start time @ the front.
+        Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
+
+     // 2a. Compare the start time of the element at the front to each element 
+            // 2b. Combine ranges that overlap
+            // 2c. Remove the old range element
+     
+    // Convert the busyTimes --> freeTimes
+    freeTimes = new ArrayList<TimeRange>();
+
+    if (busyTimes.size()==0){
+        freeTimes.add(TimeRange.fromStartEnd(0,1440,true));
+    }
+
+    int i = 0;
+    int j = 1;
+    if (busyTimes.size() == 1){ // create a time range from 0 to busyTimes[i].end)
+            freeTimes.add(TimeRange.fromStartEnd(0,busyTimes.get(i).start(),false));
+            freeTimes.add(TimeRange.fromStartEnd(busyTimes.get(i).end(),1440,false));
+        }
+
+    if (busyTimes.size() >= 2){
+        if (i==0){ // on the first time range. (0 to ...)
+            freeTimes.add(TimeRange.fromStartEnd(0,busyTimes.get(i).start(),true));
+            i++;
+        }
+        while(j != busyTimes.size()){
+            freeTimes.add(TimeRange.fromStartEnd(busyTimes.get(j).end(),busyTimes.get(i).start(),true));
+            i++;
+            j++;
+        }
+        if (j==busyTimes.size()){ // on the last time range. (... to 1440)
+            freeTimes.add(TimeRange.fromStartEnd(busyTimes.get(j).end(),1439,false));
+        }
+    }
+    
+     return freeTimes;
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
   }
 }
